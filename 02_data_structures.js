0eccccf1a1913def716b73ecd17a546f7fe7a8e2
edward <edward_gio@hotmail.com>
2026-05-27 19:53:33 -0500

    String and object

--- 02_data_structures.js
+++ 02_data_structures.js
@@ -5,3 +5,38 @@ numbers.push(40);
 console.log(numbers[2]);
 numbers.pop();
 console.log(numbers[2]);
+
+// 2. String
+const word = "hello";
+console.log(word);
+
+// 2.2 Go through string
+for (let i = 0; i < word.length; i++) {
+  console.log(word[i]);
+}
+
+// 3. Object
+const user = {
+  name: "dwaxgio",
+  isAdmin: true,
+};
+console.log(user.name);
+console.log(user.isAdmin);
+
+// 3.2 Frequency count
+const frequencyText = "hello world";
+
+function countCharacter(text, character) {
+  let count = 0;
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+
+    if (char === character) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countCharacter(frequencyText, "o")); // 2
